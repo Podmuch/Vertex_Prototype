45509cbb7d5b01f48bed469aa0eafa66cbfea143
Dominik <dominikpielak@gmail.com>
2015-11-18 11:17:26 +0100

    Init commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    #region SCENE REFERENCES
+
+    public Transform Block1;
+    public Transform Block2;
+    public Transform Block3;
+    public Transform Block4;
+
+    #endregion
+
+    private void Start () 
+    {
+	
+	}
+	
+	private void Update () 
+    {
+	
+	}
+}
